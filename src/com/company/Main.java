d64edcb2b5a765f1d513a3d32274275211ac91d4
BennoAlif <bennoalif41@gmail.com>
2019-09-25 12:03:34 +0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+    /*
+    Nama        : Benno Alif Anggara
+    Kelas       : IF1
+    NIM         : 10118024
+    Deskripsi   : Menampilkan Biodata
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------------------------");
+        System.out.println("NAMA\t\t: BENNO ALIF ANGGARA");
+        System.out.println("ALAMAT\t\t: Jl. Sekeloa Utara, Coblong");
+        System.out.println("TEMPAT LAHIR\t: SUBANG");
+        System.out.println("TANGGAL LAHIR\t: 3 AGUSTUS 2000");
+        System.out.println("---------------------------------------");
+        System.out.println("HOBI\t\t: GAMING");
+        System.out.println("MAKES\t\t: SEMUA OLAHAN PISANG");
+        System.out.println("MIKES\t\t: COCONUT LATTE A.K.A BAJIGUR");
+    }
+}
